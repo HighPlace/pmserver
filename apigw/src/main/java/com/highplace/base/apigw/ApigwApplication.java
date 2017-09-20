1d83195f672b16f2d81f36456cf1c3bf23ffed75
xfan2001 <xfan@vip.qq.com>
2017-09-20 16:18:10 +0800

    add config refresh support

--- apigw/src/main/java/com/highplace/base/apigw/ApigwApplication.java
+++ apigw/src/main/java/com/highplace/base/apigw/ApigwApplication.java
@@ -1,16 +1,28 @@
 package com.highplace.base.apigw;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@RefreshScope
 public class ApigwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigwApplication.class, args);
 	}
+
+    @Value("${hello.str}")
+    private String hello;
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return hello;
+    }
 }
