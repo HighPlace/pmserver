e8d4bf68fa9fcf26949f08c290c79537b2053a18
xfan2001 <xfan@vip.qq.com>
2017-09-21 15:20:54 +0800

    add oauth

--- oauth/src/main/java/com/highplace/service/oauth/OauthApplication.java
+++ oauth/src/main/java/com/highplace/service/oauth/OauthApplication.java
@@ -1,21 +1,26 @@
 package com.highplace.service.oauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -30,6 +35,50 @@ public class OauthApplication {
     @EnableAuthorizationServer
     protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Autowired
+        private DataSource dataSource;
+
+        @Bean // 声明TokenStore实现
+        public TokenStore tokenStore() {
+            return new JdbcTokenStore(dataSource);
+        }
+
+        @Bean // 声明 ClientDetails实现
+        public ClientDetailsService clientDetails() {
+            return new JdbcClientDetailsService(dataSource);
+        }
+
+        @Override // 配置框架应用上述实现
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+            endpoints.tokenStore(tokenStore());
+
+            // 配置TokenServices参数
+            DefaultTokenServices tokenServices = new DefaultTokenServices();
+            tokenServices.setTokenStore(endpoints.getTokenStore());
+            tokenServices.setSupportRefreshToken(false);
+            tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+            tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+            tokenServices.setAccessTokenValiditySeconds( (int) TimeUnit.DAYS.toSeconds(30)); // 30天
+            endpoints.tokenServices(tokenServices);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            //oauthServer.checkTokenAccess("isAuthenticated()");
+            oauthServer.checkTokenAccess("permitAll()");
+            oauthServer.allowFormAuthenticationForClients();
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.withClientDetails(clientDetails());
+        }
+
+        /*
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
@@ -42,6 +91,7 @@ public class OauthApplication {
                         .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
                         .scopes("app");                             // 允许的授权范围
         }
+        */
 
     }
 }
