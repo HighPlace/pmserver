a7845ff0ce57a1d122af50237d498ecd4bf4aacf
xfan2001 <xfan@vip.qq.com>
2017-09-28 00:01:35 +0800

    add unilogin

--- oauth/src/main/java/com/highplace/service/oauth/dao/ActionDao.java
+++ oauth/src/main/java/com/highplace/service/oauth/dao/ActionDao.java
@@ -2,6 +2,7 @@ package com.highplace.service.oauth.dao;
 
 import com.highplace.service.oauth.domain.Action;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -10,8 +11,8 @@ import java.util.List;
 public interface ActionDao {
 
     @Select("Select a.* from t_action a, t_role_action b where b.role_id = #{roleId} and  a.action_id = b.action_id and a.module_id=b.module_id")
-    public List<Action> findByRoleId(long roleId);
+    public List<Action> findByRoleId(@Param("roleId") long roleId);
 
     @Select("select c.* from t_user_role a, t_role_action b, t_action c where a.user_id = #{userId} and a.role_id = b.role_id and b.action_id = c.action_id and b.module_id=c.module_id")
-    public List<Action> findByUserId(long userId);
+    public List<Action> findByUserId(@Param("userId") long userId);
 }
--- oauth/src/main/java/com/highplace/service/oauth/dao/UserDao.java
+++ oauth/src/main/java/com/highplace/service/oauth/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.highplace.service.oauth.dao;
 
 import com.highplace.service.oauth.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -10,7 +11,7 @@ public interface UserDao {
     //@Select("SELECT * from t_user where username = #{username} and product_inst_id = #{productInstId}")
     //public User findByUserName(String username, String productInstId);
 
-    @Select("SELECT * from t_user where username = #{username}")
-    public User findByUsername(String username);
+    @Select("SELECT * from t_user where username = #{username} limit 1")
+    public User findByUsername(@Param("username") String username);
 
 }
