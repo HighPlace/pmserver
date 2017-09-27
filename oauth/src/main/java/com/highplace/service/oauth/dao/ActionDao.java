8365d235cdff6932bea3b977c6712e5228fef015
xfan2001 <xfan@vip.qq.com>
2017-09-28 00:57:40 +0800

    add unilogin

--- oauth/src/main/java/com/highplace/service/oauth/dao/ActionDao.java
+++ oauth/src/main/java/com/highplace/service/oauth/dao/ActionDao.java
@@ -1,18 +1,26 @@
 package com.highplace.service.oauth.dao;
 
 import com.highplace.service.oauth.domain.Action;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface ActionDao {
 
-    @Select("Select a.* from t_action a, t_role_action b where b.role_id = #{roleId} and  a.action_id = b.action_id and a.module_id=b.module_id")
-    public List<Action> findByRoleId(@Param("roleId") long roleId);
+    //@Select("Select a.* from t_action a, t_role_action b where b.role_id = #{roleId} and  a.action_id = b.action_id and a.module_id=b.module_id")
+    //public List<Action> findByRoleId(@Param("roleId") long roleId);
 
     @Select("select c.* from t_user_role a, t_role_action b, t_action c where a.user_id = #{userId} and a.role_id = b.role_id and b.action_id = c.action_id and b.module_id=c.module_id")
+    @Results({
+            @Result(id=true, column="action_id", property="actionId"),
+            @Result(column="module_id", property="moduleId"),
+            @Result(column="action_name", property="actionName"),
+            @Result(column="resource_url", property="resourceUrl"),
+            @Result(column="http_method", property="httpMethod"),
+            @Result(column="remark", property="remark"),
+            @Result(column="create_time", property="createTime"),
+            @Result(column="modify_time", property="modifyTime")
+    })
     public List<Action> findByUserId(@Param("userId") long userId);
 }
--- oauth/src/main/java/com/highplace/service/oauth/dao/UserDao.java
+++ oauth/src/main/java/com/highplace/service/oauth/dao/UserDao.java
@@ -1,9 +1,7 @@
 package com.highplace.service.oauth.dao;
 
 import com.highplace.service.oauth.domain.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserDao {
@@ -12,6 +10,22 @@ public interface UserDao {
     //public User findByUserName(String username, String productInstId);
 
     @Select("SELECT * from t_user where username = #{username} limit 1")
+    @Results({
+            @Result(id=true, column="user_id", property="userId"),
+            @Result(column="product_inst_id", property="productInstId"),
+            @Result(column="username", property="username"),
+            @Result(column="mobile_no", property="mobileNo"),
+            @Result(column="email", property="email"),
+            @Result(column="wx_open_id", property="wxOpenId"),
+            @Result(column="password", property="password"),
+            @Result(column="credential_expired", property="credentialExpired"),
+            @Result(column="account_expired", property="accountExpired"),
+            @Result(column="account_locked", property="accountLocked"),
+            @Result(column="enabled", property="enabled"),
+            @Result(column="super_user_flag", property="superUserFlag"),
+            @Result(column="create_time", property="createTime"),
+            @Result(column="modify_time", property="modifyTime")
+    })
     public User findByUsername(@Param("username") String username);
 
 }
