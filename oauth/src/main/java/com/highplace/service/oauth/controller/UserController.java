8de66437c8f7129fc59445f8d175fd492bcc1d84
xfan2001 <xfan@vip.qq.com>
2017-09-28 23:55:59 +0800

    add unilogin

--- oauth/src/main/java/com/highplace/service/oauth/controller/UserController.java
+++ oauth/src/main/java/com/highplace/service/oauth/controller/UserController.java
@@ -2,6 +2,9 @@ package com.highplace.service.oauth.controller;
 
 import com.highplace.service.oauth.dao.UserDao;
 import com.highplace.service.oauth.domain.User;
+import com.highplace.service.oauth.domain.UserView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,8 @@ import static com.highplace.service.oauth.domain.RegType.*;
 
 @RestController
 public class UserController {
+
+    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
     /*
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
@@ -45,19 +50,21 @@ public class UserController {
     //注册时一定传入实例ID regType为0,设置实例ID为空，表示是租户
     @Transactional
     @RequestMapping(path = "/reg", method = RequestMethod.POST)
-    public User createUser(@Valid @RequestBody User user) {
+    public UserView createUser(@Valid @RequestBody User user) {
 
         User existing = userDao.findByUsername(user.getUsername());
         Assert.isNull(existing, "user already exists: " + user.getUsername());
 
-        if(existing != null) {
+        if(existing == null) {
             //String hash = encoder.encode(oldUser.getPassword());
             //user.setPassword(hash);
             user.setProductInstId("550E8400-E29B-11D4-A716-446655440000");//hard code...
             userDao.insertUser(user);
+            logger.info("XXXXXXXXXXXXX  userid:" + user.getUserId());
             userDao.insertUserRole(user.getUserId(), 1L); //hard code...
+            return new UserView(user.getProductInstId(),user.getUserId(),user.getUsername());
         }
-        return user;
+        return null;
     }
 
     /*
---
+++ oauth/src/main/java/com/highplace/service/oauth/domain/UserView.java
@@ -0,0 +1,38 @@
+package com.highplace.service.oauth.domain;
+
+public class UserView {
+
+    private String productInstId;
+    private Long userId;
+    private String username;
+
+    public UserView(String productInstId, Long userId, String username) {
+        this.productInstId = productInstId;
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public String getProductInstId() {
+        return productInstId;
+    }
+
+    public void setProductInstId(String productInstId) {
+        this.productInstId = productInstId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
