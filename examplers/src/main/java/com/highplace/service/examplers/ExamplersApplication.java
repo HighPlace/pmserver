678b07c256dfc01f07a975290ec11c19d53f2052
xfan2001 <xfan@vip.qq.com>
2017-09-29 12:15:59 +0800

    add unilogin

--- examplers/src/main/java/com/highplace/service/examplers/ExamplersApplication.java
+++ examplers/src/main/java/com/highplace/service/examplers/ExamplersApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,8 @@ import java.security.Principal;
 @EnableResourceServer
 @EnableDiscoveryClient
 @RefreshScope
-public class ExamplersApplication {
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class ExamplersApplication  extends ResourceServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamplersApplication.class, args);
