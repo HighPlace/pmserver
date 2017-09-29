52686da09f5b591bb196d1494f78a308894cdb48
xfan2001 <xfan@vip.qq.com>
2017-09-29 12:31:20 +0800

    add unilogin

--- examplers/src/main/java/com/highplace/service/examplers/ExamplersApplication.java
+++ examplers/src/main/java/com/highplace/service/examplers/ExamplersApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +33,26 @@ public class ExamplersApplication  extends ResourceServerConfigurerAdapter {
     @RequestMapping(path = "/current", method = RequestMethod.GET)
     public Principal getCurrentAccount(Principal principal) {
         return principal;
+    }
+
+    @Bean
+    public RemoteTokenServices remoteTokenServices() {
+        RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+        remoteTokenServices.setCheckTokenEndpointUrl("http://172.16.0.2:11433/uaa/oauth/check_token");
+        return remoteTokenServices;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        System.out.println("==========================Configuring ResourceServerSecurityConfigurer ");
+        resources.resourceId("example-service");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/current").authenticated();
+        //.antMatchers(HttpMethod.POST, "/foo").hasAuthority("FOO_WRITE");
+        //you can implement it like this, but I show method invocation security on write
     }
 
 }
