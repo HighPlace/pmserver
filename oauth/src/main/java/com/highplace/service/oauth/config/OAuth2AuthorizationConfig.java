7fa0aa6590f35718eaa4b32d84fbbefa94006cac
xfan2001 <xfan@vip.qq.com>
2017-09-29 16:20:00 +0800

    add unilogin

--- oauth/src/main/java/com/highplace/service/oauth/config/OAuth2AuthorizationConfig.java
+++ oauth/src/main/java/com/highplace/service/oauth/config/OAuth2AuthorizationConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -30,7 +31,7 @@ import java.util.concurrent.TimeUnit;
 public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
-    @Qualifier("authenticationManagerBean")
+    //@Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
 
     @Autowired
