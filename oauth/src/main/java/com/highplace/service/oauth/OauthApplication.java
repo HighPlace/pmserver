bc715dc0abfb8853d5d2b609e1173b9aa672f871
xfan2001 <xfan@vip.qq.com>
2017-10-01 00:18:25 +0800

    add unilogin

--- oauth/src/main/java/com/highplace/service/oauth/OauthApplication.java
+++ oauth/src/main/java/com/highplace/service/oauth/OauthApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,5 +19,19 @@ public class OauthApplication {
     public static void main(String[] args) {
         SpringApplication.run(OauthApplication.class, args);
     }
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            //http.antMatcher("/user").authorizeRequests().anyRequest().authenticated();
+
+            http.authorizeRequests().antMatchers("/" , "/reg").permitAll();
+            // @formatter:on
+        }
+    }
+
 }
 
