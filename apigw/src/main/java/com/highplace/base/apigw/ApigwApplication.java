29eae9a4c505a47f6a955c28f4307b92b9528d21
xfan2001 <xfan@vip.qq.com>
2017-09-30 14:26:01 +0800

    add unilogin

--- apigw/src/main/java/com/highplace/base/apigw/ApigwApplication.java
+++ apigw/src/main/java/com/highplace/base/apigw/ApigwApplication.java
@@ -3,9 +3,17 @@ package com.highplace.base.apigw;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableZuulProxy
 @RestController
 @RefreshScope
-public class ApigwApplication {
+@EnableOAuth2Sso
+public class ApigwApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigwApplication.class, args);
@@ -26,5 +35,24 @@ public class ApigwApplication {
     @RequestMapping(value = "/hello")
     public String hello(){
         return hello;
+    }
+
+    @Bean
+    protected OAuth2RestTemplate OAuth2RestTemplate(
+            OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+        return new OAuth2RestTemplate(resource, context);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests()
+                .antMatchers("/index.html", "/home.html", "/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        // @formatter:on
     }
 }
