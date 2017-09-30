c59b8dcd634b246952ff20e6b447f288b485eb2f
xfan2001 <xfan@vip.qq.com>
2017-09-30 15:10:04 +0800

    add unilogin

--- apigw/src/main/java/com/highplace/base/apigw/App.java
+++ apigw/src/main/java/com/highplace/base/apigw/App.java
@@ -1,42 +1,24 @@
-/*
 package com.highplace.base.apigw;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableZuulProxy
-@RestController
-@RefreshScope
 @EnableOAuth2Sso
-public class ApigwApplication {
+public class App extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApigwApplication.class, args);
-	}
-
-    @Value("${hello.str}")
-    private String hello;
-
-    @RequestMapping(value = "/hello")
-    public String hello(){
-        return hello;
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 
     @Bean
@@ -57,6 +39,4 @@ public class ApigwApplication {
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         // @formatter:on
     }
-
 }
-*/
