d39e9c2c2ad9013cf9716fdeb5238cf220b22927
xfan2001 <xfan@vip.qq.com>
2017-10-19 10:44:35 +0800

    add pmserver

--- oauth/src/main/java/com/highplace/service/oauth/config/KaptchaConfig.java
+++ oauth/src/main/java/com/highplace/service/oauth/config/KaptchaConfig.java
@@ -37,7 +37,6 @@ public class KaptchaConfig {
     @Bean(name="captchaProducer")
     public DefaultKaptcha getKaptchaBean(){
         DefaultKaptcha defaultKaptcha=new DefaultKaptcha();
-        /*
         Properties properties=new Properties();
         properties.setProperty("kaptcha.border", "yes");
         properties.setProperty("kaptcha.border.color", "105,179,90");
@@ -47,12 +46,12 @@ public class KaptchaConfig {
         properties.setProperty("kaptcha.image.width", "200");
         properties.setProperty("kaptcha.image.height", "50");
         properties.setProperty("kaptcha.session.key", "code");
-        properties.setProperty("kaptcha.textproducer.font.size", "45");
+        properties.setProperty("kaptcha.textproducer.font.size", "50");
         properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.textproducer.char.space", "3");
         properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
         Config config=new Config(properties);
         defaultKaptcha.setConfig(config);
-        */
         return defaultKaptcha;
     }
 }
