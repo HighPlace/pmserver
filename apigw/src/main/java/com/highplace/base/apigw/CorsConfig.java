16e70199989bf6c08f4aeba70f42cd5ebbfbe9bb
xfan2001 <xfan@vip.qq.com>
2017-10-31 21:46:55 +0800

    add pmserver

--- apigw/src/main/java/com/highplace/base/apigw/CorsConfig.java
+++ apigw/src/main/java/com/highplace/base/apigw/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.highplace.service.oauth.config;
+package com.highplace.base.apigw;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
