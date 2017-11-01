1b978e81c20ab0306fd5d952569bbb25d4a5dea5
xfan2001 <xfan@vip.qq.com>
2017-11-01 13:17:45 +0800

    add pmserver

--- pm/src/main/java/com/highplace/biz/pm/PmApplication.java
+++ pm/src/main/java/com/highplace/biz/pm/PmApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,6 +16,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableGlobalMethodSecurity(prePostEnabled=true)   //PreAuthorize时必须开启
 @MapperScan("com.highplace.biz.pm.dao")
 @EnableScheduling  //支持定时任务
+@EnableFeignClients
+@EnableOAuth2Client
 public class PmApplication {
 
 	public static void main(String[] args) {
