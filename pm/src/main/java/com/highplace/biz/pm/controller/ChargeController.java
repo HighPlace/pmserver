5157da185820bbb3243f9a2e27348ae96dda56cb
xfan <xfan@vip.qq.com>
2017-11-10 13:37:21 +0800

    add pmserver

--- pm/src/main/java/com/highplace/biz/pm/controller/ChargeController.java
+++ pm/src/main/java/com/highplace/biz/pm/controller/ChargeController.java
@@ -178,7 +178,7 @@ public class ChargeController {
         if (charge.getChargeId() == null) throw new Exception("chargeId is null");
         String productInstId = SecurityUtils.getCurrentProductInstId(principal);
         Integer status = charge.getStatus();
-        if(status!= null && (status != 1 || status != 3)) {
+        if(status!= null && (status == 1 || status == 3)) {
             throw new Exception("出账单状态只能修改为1和3");
         }
 
