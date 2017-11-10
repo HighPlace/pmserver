d2430deb469a6ce337b2887d47d9e7544c707037
xfan <xfan@vip.qq.com>
2017-11-10 18:02:56 +0800

    add pmserver

--- pm/src/main/java/com/highplace/biz/pm/config/RedisTemplateConfig.java
+++ pm/src/main/java/com/highplace/biz/pm/config/RedisTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,9 +17,8 @@ public class RedisTemplateConfig {
     {
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
         template.setConnectionFactory(redisConnectionFactory);
-        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
-        template.setKeySerializer(stringSerializer );
-        //template.setValueSerializer(stringSerializer );
+        template.setKeySerializer(new StringRedisSerializer() );
+        template.setValueSerializer(new JdkSerializationRedisSerializer() );
         //template.setHashKeySerializer(stringSerializer );
         //template.setHashValueSerializer(stringSerializer );
         return  template;
